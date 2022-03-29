60e1fe8056394e25b2ef1aa197afa57729e93931
Andree <sm.aandree@gmail.com>
2022-03-29 10:20:17 -0500

    removed wp emojis libs

--- functions.php
+++ functions.php
@@ -60,3 +60,32 @@ if ( file_exists( __DIR__.'/vendor/cmb2/init.php' ) ) {
 include_once( __DIR__.'/inc/theme-settings/functions.php' );
 include_once( __DIR__.'/inc/theme-settings/theme-options.php' );
 include_once( __DIR__.'/inc/theme-settings/page-options.php' );
+
+
+/**
+ * Disable the emoji's
+ */
+function sast_disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'sast_disable_emojis_tinymce' );
+}
+add_action( 'init', 'sast_disable_emojis' );
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function sast_disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
